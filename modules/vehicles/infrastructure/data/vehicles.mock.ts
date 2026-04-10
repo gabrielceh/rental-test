808aab208424dbcd08b4a5d2761d16c61fee9e58
gabrielceh <gabrielcehu@gmail.com>
2026-04-10 08:00:27 -0500

    separando la data del repository.impl.ts

---
+++ modules/vehicles/infrastructure/data/vehicles.mock.ts
@@ -0,0 +1,26 @@
+export const vehiclesMockData = [
+  {
+    id: '1',
+    brand: 'Mercedes',
+    name: 'GLC 300',
+    price: 700000,
+    imageUrl:
+      'https://www.mbusa.com/content/dam/mb-nafta/us/my26-global-nav-jelly/GLC-300-4M-COUPE-GN-VDD.png',
+  },
+  {
+    id: '2',
+    brand: 'BMW',
+    name: 'X3',
+    price: 450000,
+    imageUrl:
+      'https://tiendaonlinebmw.vtexassets.com/arquivos/ids/172529/BMW-X3-M50-xDrive-Edicion-M-PRO-2026.jpg',
+  },
+  {
+    id: '3',
+    brand: 'Toyota',
+    name: 'Corolla Cross',
+    price: 380000,
+    imageUrl:
+      'https://distoyota.com/wp-content/uploads/2024/01/Corolla-Cross_super-blanco.png',
+  },
+]
--- modules/vehicles/infrastructure/repositories/mock-vehicles.repository.impl.ts
+++ modules/vehicles/infrastructure/repositories/mock-vehicles.repository.impl.ts
@@ -1,39 +1,14 @@
 import { VehiclesRepository } from '@/modules/vehicles/domain/repository';
 import { Vehicle } from '@/modules/vehicles/domain/entities';
+import { vehiclesMockData } from '@/modules/vehicles/infrastructure/data/vehicles.mock';
 
-export class MockVehiclesRepositoryImpl  implements VehiclesRepository {
-
+export class MockVehiclesRepositoryImpl implements VehiclesRepository {
   async getVehicles(): Promise<Vehicle[]> {
     // Simulamos api con delay de 1s
     return await new Promise((resolve) => {
       setTimeout(() => {
-        resolve([
-          {
-            id: '1',
-            brand: 'Mercedes',
-            name: 'GLC 300',
-            price: 700000,
-            imageUrl:
-              'https://www.mbusa.com/content/dam/mb-nafta/us/my26-global-nav-jelly/GLC-300-4M-COUPE-GN-VDD.png',
-          },
-          {
-            id: '2',
-            brand: 'BMW',
-            name: 'X3',
-            price: 450000,
-            imageUrl:
-              'https://tiendaonlinebmw.vtexassets.com/arquivos/ids/172529/BMW-X3-M50-xDrive-Edicion-M-PRO-2026.jpg',
-          },
-          {
-            id: '3',
-            brand: 'Toyota',
-            name: 'Corolla Cross',
-            price: 380000,
-            imageUrl:
-              'https://distoyota.com/wp-content/uploads/2024/01/Corolla-Cross_super-blanco.png',
-          },
-        ]);
+        resolve(vehiclesMockData);
       }, 1000);
-    })
+    });
   }
 }
